178a8589f6bd546d0bd09b8b03a7c56fb2c61911
KsenyaGorbovtsova <32801824+KsenyaGorbovtsova@users.noreply.github.com>
2019-03-22 11:14:06 +0300

    added date to userPractice

--- Sources/App/Models/UserPracticeConnection.swift
+++ Sources/App/Models/UserPracticeConnection.swift
@@ -11,6 +11,8 @@ import FluentPostgreSQL
 final class UserPracticeConnection: PostgreSQLPivot {
     
     var id: Int?
+    var date: Date
+    var repeatAfter: Int
     
     static var leftIDKey: WritableKeyPath<UserPracticeConnection, UUID> = \.userId
     static var rightIDKey: WritableKeyPath<UserPracticeConnection, UUID> = \.practiceId
@@ -20,9 +22,11 @@ final class UserPracticeConnection: PostgreSQLPivot {
     var userId: UUID
     var practiceId: UUID
     
-    init (left: User, right: Practice) throws {
+    init (left: User, right: Practice, date: Date, repeatAfter: Int) throws {
         self.userId = try left.requireID()
         self.practiceId = try right.requireID()
+        self.date = date
+        self.repeatAfter = repeatAfter
     }
     
 }
@@ -32,9 +36,9 @@ extension User {
     var containg: Siblings <User, Practice, UserPracticeConnection> {
         return self.siblings(\UserPracticeConnection.userId, \UserPracticeConnection.practiceId)
     }
-    func addPractice (practice: Practice, on connection: DatabaseConnectable) -> Future<(current: User, containing: Practice)> {
+    func addPractice (date: Date, repeatAfter: Int, practice: Practice, on connection: DatabaseConnectable) -> Future<(current: User, containing: Practice)> {
         return Future.flatMap(on: connection) {
-            let pivot = try UserPracticeConnection(left: self, right: practice)
+            let pivot = try UserPracticeConnection(left: self, right: practice, date: date, repeatAfter: repeatAfter )
             return pivot.save(on: connection).transform(to: (self, practice))
         }
     }
