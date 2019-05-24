38410c71e3254186bd586f53b367d0b2bbcd6e52
KsenyaGorbovtsova <32801824+KsenyaGorbovtsova@users.noreply.github.com>
2019-05-24 09:26:23 +0300

    repeatAfter test

--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -3,4 +3,27 @@ import Vapor
 /// Called after your application has initialized.
 public func boot(_ app: Application) throws {
     // your code here
+    app.eventLoop.scheduleRepeatedTask(initialDelay: .seconds(0), delay: .hours(24)) { task in
+        app.withPooledConnection(to: .psql) { conn -> Future<Void> in
+            let formatter = DateFormatter()
+            formatter.calendar = Calendar(identifier: .iso8601)
+            formatter.dateFormat = "yyyy-MM-dd'T'00:00:00'Z'"
+            let today = formatter.string(from: Date())
+            let todayDate = formatter.date(from: today)
+            let practices = Practice.query(on: conn).filter(\.date == todayDate).all()
+            
+            return practices.flatMap { practices -> Future<Void> in
+                practices.map { practice in
+                    let newPractice = Practice(status: practice.status, name: practice.name, owner: practice.owner, date: Calendar.current.date(byAdding: .day, value: practice.repeatAfter, to: practice.date), repeatAfter: practice.repeatAfter)
+                   /* practice.date = Calendar.current.date(byAdding: .day, value: practice.repeatAfter, to: practice.date)*/
+                    return newPractice.save(on: conn)
+                    }
+                    .flatten(on: app)
+                    .transform(to: ())
+            }
+        }
+    }
+ 
+    
 }
+
