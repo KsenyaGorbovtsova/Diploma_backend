46481581c71d1c747d96ad98857285892cdcaac2
KsenyaGorbovtsova <32801824+KsenyaGorbovtsova@users.noreply.github.com>
2019-05-24 12:32:18 +0300

    repeatAfter test

--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -18,7 +18,7 @@ public func boot(_ app: Application) throws {
                 practices.map { practice in
                      let newPractice = Practice(status: practice.status ?? false, name: practice.name ?? "Без названия", owner: practice.owner, date: Calendar.current.date(byAdding: .day, value: practice.repeatAfter ?? 0, to: practice.date ?? Date.distantPast) ?? Date.distantPast, repeatAfter: practice.repeatAfter ?? 0)
                     newPractice.save(on: conn)
-                    let users = UserPracticeConnection.query(on:conn).filter(\.practiceId == practice.id).all()
+                    let users = UserPracticeConnection.query(on:conn).filter(\.practiceId == practice.id!).all()
                     return users.flatMap { users -> Future<Void> in
                         users.map { user in
                             let newUser = user.userId
