ef78d5a48bce2bbf3dc1f3538b206239ce49cdb8
KsenyaGorbovtsova <32801824+KsenyaGorbovtsova@users.noreply.github.com>
2019-05-24 12:17:41 +0300

    repeatAfter test

--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -16,11 +16,21 @@ public func boot(_ app: Application) throws {
             
             return practices.flatMap { practices -> Future<Void> in
                 practices.map { practice in
-                   let newPractice = Practice(status: practice.status ?? false, name: practice.name ?? "Без названия", owner: practice.owner, date: Calendar.current.date(byAdding: .day, value: practice.repeatAfter ?? 0, to: practice.date ?? Date.distantPast) ?? Date.distantPast, repeatAfter: practice.repeatAfter ?? 0)
+                     let newPractice = Practice(status: practice.status ?? false, name: practice.name ?? "Без названия", owner: practice.owner, date: Calendar.current.date(byAdding: .day, value: practice.repeatAfter ?? 0, to: practice.date ?? Date.distantPast) ?? Date.distantPast, repeatAfter: practice.repeatAfter ?? 0)
+                    newPractice.save(on: conn)
+                    let users = UserPracticeConnection.query(on:conn).filter(\.practiceId == practice.id).all()
+                    return users.flatMap { users -> Future<Void> in
+                        users.map { user in
+                            let newUser = user.userId
+                           return  newUser.addPractice( practice: newPractice.id,  on: conn).transform(to: Void())
+                            
+                        }
+                    }
+                  
                     
                    /* practice.date = Calendar.current.date(byAdding: .day, value: practice.repeatAfter, to: practice.date)*/
                     
-                  //  return newPractice.save(on: conn).transform(to: Void())
+                    return 0.transform(to: Void())
                     }
                     .flatten(on: app)
                 
