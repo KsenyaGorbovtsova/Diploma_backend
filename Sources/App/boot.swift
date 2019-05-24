577d358434065eb6bb04c8c06c6ef3367a083a3c
KsenyaGorbovtsova <32801824+KsenyaGorbovtsova@users.noreply.github.com>
2019-05-24 15:37:08 +0300

    repeatAfter test

--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -6,34 +6,27 @@ import FluentSQL
 public func boot(_ app: Application) throws {
     // your code here
     app.eventLoop.scheduleRepeatedTask(initialDelay: .seconds(0), delay: .hours(24)) { task in
-        app.withPooledConnection(to: .psql) { conn -> Future<Void> in
-            let formatter = DateFormatter()
-            formatter.calendar = Calendar(identifier: .iso8601)
-            formatter.dateFormat = "yyyy-MM-dd'T'00:00:00'Z'"
-            let today = formatter.string(from: Date())
-            let todayDate = formatter.date(from: today)
-            let practices = Practice.query(on: conn).filter(\.date == todayDate).all()
-            return practices.flatMap { practices -> Future<Void> in
-                practices.map { practice in
-                   let newPractice = Practice(status: practice.status ?? false, name: practice.name ?? "Без названия", owner: practice.owner, date: Calendar.current.date(byAdding: .day, value: practice.repeatAfter ?? 0, to: practice.date ?? Date.distantPast) ?? Date.distantPast, repeatAfter: practice.repeatAfter ?? 0)
-                    let exercise = try! practice.containing.query(on: conn).all()
-                    return exercise.flatMap { exercise -> [Future<()>] in
-                        exercise.map {
-                            exr in
-                            newPractice.addExercise(exercise: exr, on: conn).transform(to: Void())
-                        }
-                        
-                    }
-                                       /* practice.date = Calendar.current.date(byAdding: .day, value: practice.repeatAfter, to: practice.date)*/
-                    
-                   return newPractice.save(on: conn).transform(to: Void())
-                    }
-                    .flatten(on: app)
+        app.eventLoop.scheduleRepeatedTask(initialDelay: .seconds(0), delay: .hours(24)) { task in
+            app.withPooledConnection(to: .psql) { conn -> Future<Void> in
+                let formatter = DateFormatter()
+                formatter.calendar = Calendar(identifier: .iso8601)
+                formatter.dateFormat = "yyyy-MM-dd'T'00:00:00'Z'"
+                let today = formatter.string(from: Date())
+                let todayDate = formatter.date(from: today)
+                let practices = Practice.query(on: conn).filter(\.date == todayDate).all()
                 
+                return practices.flatMap { practices -> Future<Void> in
+                    practices.map { practice in
+                        practice.date = Calendar.current.date(byAdding: .day, value: practice.repeatAfter, to: practice.date)
+                        return practice.update(on: conn)
+                        }
+                        .flatten(on: app)
+                        .transform(to: ())
+                }
             }
         }
     }
- 
-    
 }
+    
+
 
