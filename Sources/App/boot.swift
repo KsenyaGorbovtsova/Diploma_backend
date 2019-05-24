0a11b3030be5451e42dd834b840b35fb5122e795
KsenyaGorbovtsova <32801824+KsenyaGorbovtsova@users.noreply.github.com>
2019-05-24 10:32:14 +0300

    repeatAfter test

--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -21,7 +21,7 @@ public func boot(_ app: Application) throws {
                     return newPractice.save(on: conn)
                     }
                     .flatten(on: app)
-                    .transform(to: Practice)
+                    .transform(to: Practice.self)
             }
         }
     }
